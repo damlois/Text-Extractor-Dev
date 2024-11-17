e6642c5b62b693cea358f5e255a826643b69e02c
Taofiq Sulayman <Sulaymantaofiq@gmail.com>
2024-11-18 00:07:11 +0100

    feat: enhance fileProcessorApi with updateChatSession method and improve GenerateInsight component structure

--- src/pages/home/processFiles/tabs/GenerateInsight.tsx
+++ src/pages/home/processFiles/tabs/GenerateInsight.tsx
@@ -2,6 +2,7 @@ import React, { useState, useRef, useEffect } from "react";
 import { LoadingOutlined, SendOutlined } from "@ant-design/icons";
 import AppInput from "../../../../components/AppInput";
 import { Image, Spin } from "antd";
+import { useFileProcessor } from "../../../../context/FileProcessorContext";
 
 
 type Message = {
@@ -14,9 +15,11 @@ const GenerateInsight: React.FC = () => {
   const [messages, setMessages] = useState<Message[]>([]);
   const [input, setInput] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false); // State to track loading status
-
-
   const messageListRef = useRef<HTMLDivElement>(null);
+
+  const { currentProject } = useFileProcessor();
+
+  console.log(currentProject);
 
 
   const handleSendMessage = () => {
@@ -54,64 +57,75 @@ const GenerateInsight: React.FC = () => {
 
 
   return (
-    <div className="flex flex-col items-center mx-[11%] relative">
-      <div
-        className="chat-container flex flex-col w-full overflow-y-auto"
-        style={{ height: "calc(100vh - 80px)", paddingBottom: "80px" }}
-        ref={messageListRef}
-      >
-        {messages.map((msg, index) => (
-          <div
-            key={index}
-            className={`mt-1 ${
-              msg.from === "model" || msg.from === "loader"
-                ? "flex items-center gap-6"
-                : ""
-            }`}
-          >
-            {(msg.from === "model" || msg.from === "loader") && (
-              <Image src="/assets/icons/blue-circle-icon.svg" preview={false} />
-            )}
-            <div
-              className={`message-bubble ${
-                msg.from === "user" ? "py-2 px-4" : "py-[1px]"
-              } rounded-lg text-[14px] ${
-                msg.from === "user"
-                  ? "bg-[#f5f5f5] text-[#00000073] w-fit ml-auto text-right"
-                  : msg.from === "loader"
-                  ? "bg-transparent flex items-center"
-                  : "bg-gray-300 text-black self-start"
-              }`}
-            >
-              {msg.from === "loader" ? (
-                <Spin
-                  indicator={<LoadingOutlined style={{ fontSize: 24 }} spin />}
-                />
-              ) : (
-                msg.text
-              )}
-            </div>
+      <div className="flex flex-col items-center mx-[11%] relative">
+          <div className="text-[24px] text-black my-6">
+              Analyze and generate insights from your documents with interprAIs
           </div>
-        ))}
+          <div
+              className="chat-container flex flex-col w-full overflow-y-auto"
+              style={{ height: "calc(100vh - 80px)", paddingBottom: "80px" }}
+              ref={messageListRef}
+          >
+              {messages.map((msg, index) => (
+                  <div
+                      key={index}
+                      className={`mt-1 ${
+                          msg.from === "model" || msg.from === "loader"
+                              ? "flex items-center gap-6"
+                              : ""
+                      }`}
+                  >
+                      {(msg.from === "model" || msg.from === "loader") && (
+                          <Image
+                              src="/assets/icons/blue-circle-icon.svg"
+                              preview={false}
+                          />
+                      )}
+                      <div
+                          className={`message-bubble ${
+                              msg.from === "user" ? "py-2 px-4" : "py-[1px]"
+                          } rounded-lg text-[14px] ${
+                              msg.from === "user"
+                                  ? "bg-[#f5f5f5] text-[#00000073] w-fit ml-auto text-right"
+                                  : msg.from === "loader"
+                                  ? "bg-transparent flex items-center"
+                                  : "bg-gray-300 text-black self-start"
+                          }`}
+                      >
+                          {msg.from === "loader" ? (
+                              <Spin
+                                  indicator={
+                                      <LoadingOutlined
+                                          style={{ fontSize: 24 }}
+                                          spin
+                                      />
+                                  }
+                              />
+                          ) : (
+                              msg.text
+                          )}
+                      </div>
+                  </div>
+              ))}
+          </div>
+          <div
+              className="message-input fixed bottom-[0] pb-[60px] bg-white mx-[10%]"
+              style={{ width: "-webkit-fill-available" }}
+          >
+              <AppInput
+                  value={input}
+                  onChange={(e) => setInput(e.target.value)}
+                  placeholder="Message InterprAIs"
+                  rightIcon={<SendOutlined />}
+                  onPressEnter={handleSendMessage}
+                  loading={loading}
+                  className="mt-0"
+              />
+              <div className="flex justify-center items-center text-gray text-sm pt-[10px]">
+                  InterprAIs can make mistakes. Check important Info
+              </div>
+          </div>
       </div>
-      <div
-        className="message-input fixed bottom-[0] pb-[60px] bg-white mx-[10%]"
-        style={{ width: "-webkit-fill-available" }}
-      >
-        <AppInput
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          placeholder="Message InterprAIs"
-          rightIcon={<SendOutlined />}
-          onPressEnter={handleSendMessage}
-          loading={loading}
-          className="mt-0"
-        />
-        <div className="flex justify-center items-center text-gray text-sm pt-[10px]">
-          InterprAIs can make mistakes. Check important Info
-        </div>
-      </div>
-    </div>
   );
 };
 
