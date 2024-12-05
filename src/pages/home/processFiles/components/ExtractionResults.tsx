9a302f12ad0d21c7a93b7e956e122136d40d56c2
Lois Adegbohungbe <lois0168@gmail.com>
2024-12-05 13:32:53 +0100

    Addition of label tags

--- src/pages/home/processFiles/components/ExtractionResults.tsx
+++ src/pages/home/processFiles/components/ExtractionResults.tsx
@@ -7,6 +7,7 @@ import { useNavigate } from "react-router-dom";
 import { extractKeyValuePairs } from "../utils";
 import CustomTable from "../../../../components/CustomTable";
 import { useGetProjectAnalyses } from "../../../../hooks/useFileProcessor";
+import LabelTag from "./LabelTag";
 
 const ExtractionResults = () => {
   const [result, setResult] = useState<any[]>([]);
@@ -103,47 +104,70 @@ const ExtractionResults = () => {
       ) : (
         <>
           {result.length ? (
-            <div className="w-full border border-[#F0F0F0] rounded-sm">
-              <div className="flex justify-between py-4 px-6 border-b border-[#F0F0F0]">
-                <div className="text-dark-gray text-[16px] font-medium">
-                  Extract Preview
+            <div>
+              <div
+                className="w-full border border-[#F0F0F0] rounded-sm"
+                style={{ height: "calc(100vh - 300px)", overflowY: "scroll" }}
+              >
+                <div className="flex justify-between py-4 px-6 border-b border-[#F0F0F0]">
+                  <div className="text-dark-gray text-[16px] font-medium">
+                    Extract Preview
+                  </div>
+                  <div className="flex gap-2">
+                    <AppButton
+                      children={
+                        <div>
+                          <DownloadOutlined className="mr-2" />
+                          CSV
+                        </div>
+                      }
+                      width="fit-content"
+                      onClick={downloadAsCSV}
+                      className="p-1"
+                      variant="secondary"
+                    />
+                    <AppButton
+                      children={
+                        <div>
+                          <DownloadOutlined className="mr-2" />
+                          JSON
+                        </div>
+                      }
+                      width="fit-content"
+                      onClick={downloadAsJSON}
+                      className="p-1"
+                      variant="secondary"
+                    />
+                  </div>
                 </div>
-                <div className="flex gap-2">
-                  <AppButton
-                    children={
-                      <div>
-                        <DownloadOutlined className="mr-2" />
-                        CSV
-                      </div>
-                    }
-                    width="fit-content"
-                    onClick={downloadAsCSV}
-                    className="p-1"
-                    variant="secondary"
-                  />
-                  <AppButton
-                    children={
-                      <div>
-                        <DownloadOutlined className="mr-2" />
-                        JSON
-                      </div>
-                    }
-                    width="fit-content"
-                    onClick={downloadAsJSON}
-                    className="p-1"
-                    variant="secondary"
+                <div className="p-6">
+                  <CustomTable
+                    dataSource={result}
+                    columns={columns}
+                    rowKey="id"
+                    pagination={result.length > 7 ? { pageSize: 7 } : false}
+                    bordered
+                    striped
                   />
                 </div>
-              </div>
-              <div className="p-6">
-                <CustomTable
-                  dataSource={result}
-                  columns={columns}
-                  rowKey="id"
-                  pagination={result.length > 7 ? { pageSize: 7 } : false}
-                  bordered
-                  striped
-                />
+                {sessionType === "Existing" && (
+                  <div className="px-6 pb-6 pt-2 font-inter">
+                    <p className="text-[16px] font-medium mb-1">Label List</p>
+                    <p className="text-[14px]">
+                      Below is a compilation of the list of labels selected.
+                    </p>
+                    <div className="flex flex-wrap gap-4 items-start mt-6">
+                      {columns.map((label) => (
+                        <LabelTag
+                          key={label.key}
+                          id={Number(label.key)}
+                          name={label.title}
+                          style={{ padding: "6px 8px" }}
+                        />
+                      ))}
+                    </div>
+                  </div>
+                )}
               </div>
             </div>
           ) : (
