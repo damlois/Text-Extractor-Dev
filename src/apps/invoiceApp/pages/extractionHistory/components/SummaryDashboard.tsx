f5334d9949f2b767b5249b56745ca0ebaafa69d2
Lois Adegbohungbe <lois0168@gmail.com>
2025-01-27 14:35:01 +0100

    minor-update

--- src/apps/invoiceApp/pages/extractionHistory/components/SummaryDashboard.tsx
+++ src/apps/invoiceApp/pages/extractionHistory/components/SummaryDashboard.tsx
@@ -4,12 +4,12 @@ const SummaryDashboard = () => {
   return (
     <div className="grid gap-4 space-between flex-wrap lg:grid-cols-2 sm:grid-cols-1">
       <MetricCard
-        iconUrl="/assets/icons/dashboard-success.svg"
+        iconUrl="/assets/icons/dashboard-success-icon.svg"
         status="Successful"
         count="5,890"
       />
       <MetricCard
-        iconUrl="/assets/icons/dashboard-failed.svg"
+        iconUrl="/assets/icons/dashboard-failed-icon.svg"
         status="Failed"
         count="78990"
       />
