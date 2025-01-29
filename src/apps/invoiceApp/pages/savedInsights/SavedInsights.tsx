f72a750b40fc59c5c0079bfe33585daf47703f32
Lois Adegbohungbe <lois0168@gmail.com>
2025-01-29 15:38:19 +0100

    Saved Insights UI

--- src/apps/invoiceApp/pages/savedInsights/SavedInsights.tsx
+++ src/apps/invoiceApp/pages/savedInsights/SavedInsights.tsx
@@ -1,9 +1,144 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { Table, Dropdown, Button } from "antd";
+import type {TableColumnsType } from "antd";
+import { FaEllipsisVertical } from "react-icons/fa6";
+import FilterInsightsModal from "./components/FilterInsightsModal";
 
-const SavedInsights = () => {
-  return (
-    <div>SavedInsights</div>
-  )
+export interface SavedInsights {
+  insightSummary: string;
+  created_at: string;
 }
 
-export default SavedInsights
+const SavedInsights = () => {
+  const [loading, setLoading] = useState(false);
+  const [savedInsights, setSavedInsights] = useState<SavedInsights[]>([]);
+  const [showFilterModal, setShowFilterModal] = useState(false);
+  const [pagination, setPagination] = useState({
+    current: 1,
+    pageSize: 10,
+    total: 0,
+  });
+
+  const data = [
+    {
+      insightSummary: "Difference in payment terms across invoices",
+      created_at: "9th January 2025",
+    },
+    {
+      insightSummary: "Difference in payment terms across invoices",
+      created_at: "9th January 2025",
+    },
+    {
+      insightSummary: "Difference in payment terms across invoices",
+      created_at: "9th January 2025",
+    },
+    {
+      insightSummary: "Difference in payment terms across invoices",
+      created_at: "9th January 2025",
+    },
+    {
+      insightSummary: "Difference in payment terms across invoices",
+      created_at: "9th January 2025",
+    },
+  ];
+
+  const fetchSavedInsights = async (page: number, size: number) => {
+    setLoading(true);
+    try {
+      setSavedInsights(data);
+    } catch (error) {
+      console.error("Error fetching saved insights:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const toggleFilterModal = () => setShowFilterModal(!showFilterModal);
+
+  useEffect(() => {
+    fetchSavedInsights(1, 10);
+  }, []);
+
+  const savedInsightsColumns: TableColumnsType<SavedInsights> = [
+    {
+      title: "Saved Insights",
+      dataIndex: "insightSummary",
+      render: (text: string, record: SavedInsights) => (
+        <button className="text-dark-gray text-[14px] font-medium underline text-left">
+          {text}
+        </button>
+      ),
+    },
+    {
+      title: "Date",
+      dataIndex: "created_at",
+      render: (text: string) => (
+        <span className="text-[#28373] text-[14px]">{text}</span>
+      ),
+    },
+    {
+      title: "",
+      key: "actions",
+      align: "center" as const,
+      render: (_: any, record: SavedInsights) => (
+        <Dropdown
+          menu={{
+            items: [
+              {
+                key: "1",
+                label: (
+                  <a onClick={() => alert(`view ${record.insightSummary}`)}>
+                    View
+                  </a>
+                ),
+              },
+            ],
+          }}
+        >
+          <Button
+            icon={<FaEllipsisVertical className="text-black" />}
+            type="link"
+            className="py-2 px-3 border border-[#E4E7EC] rounded-lg"
+          />
+        </Dropdown>
+      ),
+    },
+  ];
+
+  const handleTableChange = (pagination: any) => {
+    // fetchInvoices(pagination.current, pagination.pageSize);
+  };
+
+  return (
+    <div>
+      <div
+        className="p-4 border-r border-l border-t border-[#E4E7EC]"
+        style={{ borderTop: "2px solid #E4E7EC" }}
+      >
+        <img
+          src="/assets/images/filter-btn.png"
+          alt="Filter"
+          className="cursor-pointer"
+          onClick={toggleFilterModal}
+        />
+      </div>
+      <div className="overflow-x-auto">
+        <Table<SavedInsights>
+          rowKey="id"
+          columns={savedInsightsColumns}
+          dataSource={savedInsights}
+          className="invoice-app-table extraction-history-table no-vertical-lines"
+          loading={loading}
+          pagination={pagination}
+          onChange={handleTableChange}
+        />
+      </div>
+      <FilterInsightsModal
+        open={showFilterModal}
+        onCancel={toggleFilterModal}
+      />
+    </div>
+  );
+};
+
+export default SavedInsights;
---
+++ src/apps/invoiceApp/pages/savedInsights/components/FilterInsightsModal.tsx
@@ -0,0 +1,65 @@
+import { DatePicker, Modal, Tooltip } from "antd";
+import AppButton from "../../../../../components/AppButton";
+import { InfoCircleOutlined } from "@ant-design/icons";
+
+interface FilterInsightsModalProps {
+  open: boolean;
+  onCancel: () => void;
+}
+
+const FilterInsightsModal = ({ open, onCancel }: FilterInsightsModalProps) => {
+  const { RangePicker } = DatePicker;
+
+  const handleDateChange = (dates: any) => {
+    if (dates) {
+      const [dateFrom, dateTo] = dates;
+      console.log("From:", dateFrom?.format("YYYY-MM-DD"));
+      console.log("To:", dateTo?.format("YYYY-MM-DD"));
+    } else {
+      console.log("No date selected.");
+    }
+  };
+
+  return (
+    <Modal
+      open={open}
+      onCancel={onCancel}
+      footer={null}
+      className="app-modal"
+      style={{ minWidth: '30%', top: "30%" }}
+    >
+      <div>
+        <div className="text-[20px] font-bold p-6 border-b border-0.5 border-[#cfc1c1]">
+          Filter by Date
+        </div>
+        <div className="p-6">
+          <div className={`flex items-center gap-2 mb-2 `}>
+            <p className="text-dark-gray font-bold text-[14.5px]">Date Range</p>
+
+            <Tooltip title="Date Range">
+              <InfoCircleOutlined className="text-gray-500 text-[14px] cursor-pointer" />
+            </Tooltip>
+          </div>
+          <RangePicker className="w-full h-[38px]" onChange={handleDateChange} />
+        </div>
+        <div className="border-t border-[#f0f0f0]">
+          <div className="flex flex-end gap-2 p-6 flex-wrap">
+            <AppButton
+              width="82px"
+              className="mr-0"
+              variant="secondary"
+              onClick={onCancel}
+            >
+              Clear All
+            </AppButton>
+            <AppButton width="82px" className="ml-0 mr-0">
+              Filter
+            </AppButton>
+          </div>
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default FilterInsightsModal;
