f8420daa7094a3d48de784371be55d81701465f8
imodupsy <modupeadeonojobi@gmail.com>
2025-02-11 12:50:57 +0100

    add keycloak config for demo env

--- src/service/keycloakService.js
+++ src/service/keycloakService.js
@@ -1,6 +1,16 @@
 import Keycloak from "keycloak-js";
-const _kc = new Keycloak('/keycloak.dev.json');
 
+const getKeycloakConfig = () => {
+  const environment = process.env.REACT_APP_ENV;
+  const configMap = {
+    dev: '/keycloak.dev.json',
+    demo: '/keycloak.demo.json',
+  };
+
+  return configMap[environment] || configMap.dev; 
+};
+
+const _kc = new Keycloak(getKeycloakConfig());
 
 /**
  * Initializes Keycloak instance and calls the provided callback function if successfully authenticated.
