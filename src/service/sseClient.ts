d2c817740f440ccb2b008219b21fdcac01404c92
Lois Adegbohungbe <lois0168@gmail.com>
2025-04-02 16:03:42 +0100

    SSE Issue fix

--- src/service/sseClient.ts
+++ src/service/sseClient.ts
@@ -1,19 +1,11 @@
 export const manageSSE = (endpoint: string, onMessage: (data: any) => void) => {
   const BASE_URL = process.env.REACT_APP_INVOICE_API_URL || "";
-  let isStopped = false;
-  let retryTimeout: NodeJS.Timeout | null = null;
   let eventSource: EventSource | null = null;
 
   const startSSE = () => {
-    if (isStopped) return;
     eventSource = new EventSource(`${BASE_URL}${endpoint}`);
 
     eventSource.onmessage = ({ data }: MessageEvent) => {
-      if (isStopped) {
-        eventSource?.close();
-        return;
-      }
-
       const cleanedData = data
         .split("\n")
         .map((line: string) => line.replace(/^data:\s*/, "").trim())
@@ -30,26 +22,21 @@ export const manageSSE = (endpoint: string, onMessage: (data: any) => void) => {
       }
     };
 
-    eventSource.onerror = () => {
-      if (isStopped) return;
-      console.warn("SSE connection error. Closing Connection...");
+    eventSource.addEventListener("done", () => {
       eventSource?.close();
+    });
 
-      // retryTimeout = setTimeout(startSSE, 3000);
-    };
+    eventSource.addEventListener("error", (event) => {
+      console.error("Streaming error:", event);
+      eventSource?.close();
+    });
   };
+  
   startSSE();
 
   return {
     stop: () => {
-      isStopped = true;
-      if (eventSource) {
-        eventSource.close();
-      }
-      if (retryTimeout) {
-        clearTimeout(retryTimeout);
-        retryTimeout = null;
-      }
+      eventSource?.close();
     },
   };
 };
