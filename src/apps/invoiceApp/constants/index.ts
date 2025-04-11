5454fa83cbc57b22faced565af55435210acde84
Lois Adegbohungbe <lois0168@gmail.com>
2025-04-11 07:02:05 +0100

    minor update

--- src/apps/invoiceApp/constants/index.ts
+++ src/apps/invoiceApp/constants/index.ts
@@ -101,6 +101,6 @@ export const TIMEOUT_ERROR_MESSAGE = "The request timed out. Please try again.";
 export const AUTH_IFRAME_TIMEOUT_MESSAGE =
   "Session has expired. Please log in again.";
 export const UNAUTHORIZED_MESSAGE =
-  "You are unauthorized to perform this action. Please log in again.";
+  "You are unauthorized to perform this action.";
 export const FORBIDDEN_MESSAGE =
   "You do not have permission to perform this action.";
