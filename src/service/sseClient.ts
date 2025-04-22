1461472aca5312e34e5b38145c371eb11373bd8d
Lois Adegbohungbe <lois0168@gmail.com>
2025-04-22 13:57:45 +0100

    removed timeout in SSE

--- src/service/sseClient.ts
+++ src/service/sseClient.ts
@@ -1,27 +1,14 @@
 export const manageSSE = (endpoint: string, onMessage: (data: any) => void) => {
-  const BASE_URL = process.env.REACT_APP_DEMO_API_URL || "";
+  const BASE_URL = process.env.REACT_APP_DEV_API_URL || "";
   let eventSource: EventSource | null = null;
-  let timeoutId: NodeJS.Timeout;
-
-  const TIMEOUT_MS = 10000;
 
   const startSSE = () => {
     eventSource = new EventSource(`${BASE_URL}${endpoint}`);
 
-    const handleTimeout = () => {
-      console.warn("No data received from SSE within timeout window.");
-      onMessage([]);
-      eventSource?.close();
-    };
-
-    timeoutId = setTimeout(handleTimeout, TIMEOUT_MS);
-
     eventSource.onmessage = ({ data }: MessageEvent) => {
-      clearTimeout(timeoutId);
-
       const cleanedData = data
         .split("\n")
-        .filter((line: string) => line.startsWith("data:")) // Ignore ping lines
+        .filter((line: string) => line.startsWith("data:"))
         .map((line: string) => line.replace(/^data:\s*/, "").trim())
         .filter(Boolean)
         .join("");
@@ -43,19 +30,14 @@ export const manageSSE = (endpoint: string, onMessage: (data: any) => void) => {
         console.error("Error parsing SSE data:", error);
         onMessage([]);
       }
-
-      // Optionally restart timeout to close connection again if no new messages
-      timeoutId = setTimeout(handleTimeout, TIMEOUT_MS);
     };
 
     eventSource.addEventListener("done", () => {
-      clearTimeout(timeoutId);
       eventSource?.close();
     });
 
     eventSource.addEventListener("error", (event) => {
       console.error("Streaming error:", event);
-      clearTimeout(timeoutId);
       eventSource?.close();
     });
   };
@@ -64,7 +46,6 @@ export const manageSSE = (endpoint: string, onMessage: (data: any) => void) => {
 
   return {
     stop: () => {
-      clearTimeout(timeoutId);
       eventSource?.close();
     },
   };
