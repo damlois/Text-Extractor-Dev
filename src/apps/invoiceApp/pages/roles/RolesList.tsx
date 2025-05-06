acaee8b033c8dc488b883a228b0e2fd7cc94ae23
Taofiq Sulayman <sulaymantaofiq@gmail.com>
2025-05-06 19:01:31 +0100

    Refactor icon usage in multiple components to use function calls for rendering

--- src/apps/invoiceApp/pages/roles/RolesList.tsx
+++ src/apps/invoiceApp/pages/roles/RolesList.tsx
@@ -57,7 +57,6 @@ const RolesList = () => {
     toggleModal("edit_role");
   };
 
-
   const rolesListColumns: TableColumnsType<RoleResponse> = [
     {
       title: "Role Title",
@@ -115,7 +114,7 @@ const RolesList = () => {
                 }}
               >
                 <Button
-                  icon={<FaEllipsisVertical className="text-black" />}
+                  icon={FaEllipsisVertical({ className: "text-black" })}
                   type="link"
                   className="py-2 px-3 border border-[#E4E7EC] rounded-lg"
                 />
