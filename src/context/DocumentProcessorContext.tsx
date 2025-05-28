646f9d795e496e54c096ea879dfe60ab6e5a4074
Lois Adegbohungbe <lois0168@gmail.com>
2025-05-28 08:12:54 +0100

    integrate Retry permission

---
+++ src/context/DocumentProcessorContext.tsx
@@ -0,0 +1,135 @@
+import {
+  createContext,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useContext,
+  useState,
+} from "react";
+import { DataSourceDetails, ProcessedDocument } from "../types";
+import {
+  DuplicateDocumentItemResponse,
+  DuplicateDocumentsFileHashMap,
+  ItemField,
+  RegularField,
+} from "../pages/app/extractionHistory/types";
+import { processorApi } from "../api";
+import { useApplication } from "./ApplicationContext";
+
+interface DocumentProcessorContextProps {
+  currentDataSource: DataSourceDetails | undefined;
+  setCurrentDataSource: Dispatch<SetStateAction<DataSourceDetails | undefined>>;
+  documentsMapById: Record<string, ProcessedDocument>;
+  setDocumentsMapById: Dispatch<
+    SetStateAction<Record<string, ProcessedDocument>>
+  >;
+  duplicatesMapById: Record<string, DuplicateDocumentItemResponse> | null;
+  setDuplicatesMapById: Dispatch<
+    SetStateAction<Record<string, DuplicateDocumentItemResponse>>
+  >;
+  duplicateMapByFileHash: DuplicateDocumentsFileHashMap;
+  setDuplicatesMapByFileHash: Dispatch<
+    SetStateAction<DuplicateDocumentsFileHashMap>
+  >;
+  duplicatesRefresh: boolean | null;
+  setDuplicatesRefresh: Dispatch<SetStateAction<boolean>>;
+  duplicatesCount: number;
+  setDuplicatesCount: Dispatch<SetStateAction<number>>;
+  reviewDocument: ProcessedDocument | undefined;
+  setReviewDocument: Dispatch<SetStateAction<ProcessedDocument | undefined>>;
+  regularFieldsData: RegularField[];
+  itemsFieldsData: ItemField[];
+  setRegularFieldsData: Dispatch<SetStateAction<RegularField[]>>;
+  setItemsFieldsData: Dispatch<SetStateAction<ItemField[]>>;
+  loadingDataSource: boolean;
+  fetchDataSource: () => Promise<void>;
+}
+
+interface DocumentProcessorProviderProps {
+  children: ReactNode;
+}
+
+const DocumentProcessorContext = createContext<
+  DocumentProcessorContextProps | undefined
+>(undefined);
+
+export const DocumentProcessorProvider: React.FC<
+  DocumentProcessorProviderProps
+> = ({ children }) => {
+  const [currentDataSource, setCurrentDataSource] = useState<
+    DataSourceDetails | undefined
+  >(undefined);
+  const [documentsMapById, setDocumentsMapById] = useState<
+    Record<string, ProcessedDocument>
+  >({});
+  const [duplicatesMapById, setDuplicatesMapById] = useState<
+    Record<string, DuplicateDocumentItemResponse>
+  >({});
+  const [duplicateMapByFileHash, setDuplicatesMapByFileHash] =
+    useState<DuplicateDocumentsFileHashMap>({});
+  const [duplicatesRefresh, setDuplicatesRefresh] = useState<boolean>(false);
+  const [duplicatesCount, setDuplicatesCount] = useState(0);
+  const [reviewDocument, setReviewDocument] = useState<
+    ProcessedDocument | undefined
+  >();
+  const [regularFieldsData, setRegularFieldsData] = useState<RegularField[]>(
+    []
+  );
+  const [itemsFieldsData, setItemsFieldsData] = useState<ItemField[]>([]);
+
+  const [loadingDataSource, setLoadingDataSource] = useState(true);
+
+  const { documentType } = useApplication();
+
+  const fetchDataSource = async () => {
+    try {
+      setLoadingDataSource(true);
+      const response = await processorApi.getDataSourceDetails(documentType);
+      const data = response.data.data;
+      setCurrentDataSource(data[data.length - 1]);
+    } catch (error) {
+      console.error("Error fetching data source details:", error);
+    } finally {
+      setLoadingDataSource(false);
+    }
+  };
+
+  return (
+    <DocumentProcessorContext.Provider
+      value={{
+        currentDataSource,
+        setCurrentDataSource,
+        documentsMapById,
+        setDocumentsMapById,
+        duplicatesMapById,
+        setDuplicatesMapById,
+        duplicateMapByFileHash,
+        setDuplicatesMapByFileHash,
+        duplicatesRefresh,
+        setDuplicatesRefresh,
+        duplicatesCount,
+        setDuplicatesCount,
+        reviewDocument,
+        setReviewDocument,
+        itemsFieldsData,
+        setItemsFieldsData,
+        regularFieldsData,
+        setRegularFieldsData,
+        loadingDataSource,
+        fetchDataSource,
+      }}
+    >
+      {children}
+    </DocumentProcessorContext.Provider>
+  );
+};
+
+export const useDocumentProcessor = () => {
+  const context = useContext(DocumentProcessorContext);
+  if (!context) {
+    throw new Error(
+      "useDocumentProcessor must be used within a DocumentProcessorProvider"
+    );
+  }
+  return context;
+};
---
+++ src/context/TemplateContext.tsx
@@ -0,0 +1,76 @@
+import { createContext, ReactNode, useContext, useState } from "react";
+import { processorApi } from "../api";
+import { TemplateItem } from "../types";
+import { handleError } from "../utils/notification";
+import { useDocumentProcessor } from "./DocumentProcessorContext";
+
+interface TemplateContextProps {
+  templateItems: TemplateItem[];
+  setTemplateItems: (items: TemplateItem[]) => void;
+  loading: boolean;
+  fetchTemplate: () => Promise<void>;
+  saveTemplate: (dataSourceId?: string) => Promise<void>;
+}
+
+const TemplateContext = createContext<TemplateContextProps | undefined>(
+  undefined
+);
+
+export const TemplateProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [templateItems, setTemplateItems] = useState<TemplateItem[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const { currentDataSource } = useDocumentProcessor();
+
+  const fetchTemplate = async () => {
+    try {
+      setLoading(true);
+      const response = await processorApi.getTemplate(
+        currentDataSource?.id
+      );
+      setTemplateItems(response.data.data.items);
+    } catch (error) {
+      handleError(error, "Template");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const saveTemplate = async () => {
+    try {
+      setLoading(true);
+      await processorApi.updateTemplate(
+        currentDataSource?.id,
+        templateItems
+      );
+    } catch (error) {
+      handleError("error", "Template");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <TemplateContext.Provider
+      value={{
+        templateItems,
+        setTemplateItems,
+        loading,
+        fetchTemplate,
+        saveTemplate,
+      }}
+    >
+      {children}
+    </TemplateContext.Provider>
+  );
+};
+
+export const useTemplate = () => {
+  const context = useContext(TemplateContext);
+  if (!context) {
+    throw new Error("useTemplate must be used within a TemplateProvider");
+  }
+  return context;
+};
